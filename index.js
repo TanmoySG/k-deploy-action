4c8131024c9c4a84765021ea0d380e53683046ea
Tanmoy Sen Gupta <tanmoysps@gmail.com>
2022-07-16 04:43:06 +0000

    Added Distribution using Vercel/Ncc

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
 const core = require('@actions/core');
 
+const fs = require('fs');
+
 
 // most @actions toolkit packages have async methods
 async function run() {
   try {
     const kubeconfigPath = core.getInput('kubeconfig');
     core.info(`Kubeconfig Path ${kubeconfigPath}.`);
+
+    fs.readFile(kubeconfigPath, 'utf8', (err, data) => {
+      if (err) {
+        console.error(err);
+        return;
+      }
+      core.info(data);
+    });
   } catch (error) {
     core.setFailed(error.message);
   }
